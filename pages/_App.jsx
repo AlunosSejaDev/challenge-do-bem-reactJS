c91c5b9894078e6942310be175bb09e7783142e6
Bruno Patrício <bpatriciocosta@gmail.com>
2022-08-04 16:23:45 -0300

    Add babel + config + tipografias

--- pages/_App.jsx
+++ pages/_App.jsx
@@ -1,10 +1,26 @@
-import '../src/style.css';
+import { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+ * {
+    padding: 0;
+    margin: 0;
+ }
+
+ body {
+  font-family: 'Roboto', sans-serif;
+ }
+`
 
 function App ({ Component, pageProps }) {
   return (
-        
+      <>
+
+      <GlobalStyle />
+
       <Component { ...pageProps }/>
+      
+      </>
+      
    
   )
 }
---
+++ src/components/typograph/H1.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  font-size: 60px;
+  font-weight: bold;
+`
+
+export default H1
