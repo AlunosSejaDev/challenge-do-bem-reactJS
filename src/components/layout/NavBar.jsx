24f2e0aadc8a505ca2492a30afb6ded1d344ee4d
Bruno Patrício <bpatriciocosta@gmail.com>
2022-08-10 16:01:56 -0300

    Ajuste de responsividade/exc conteiner + linebar

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -3,17 +3,29 @@ import styled from "styled-components";
 const StyledNavBar = styled.div`
   background-color: ${props => props.theme.primary};
   height: 80px;
-  max-width: 100%;
   display: flex;
   align-items: center;
   padding: 10px 20px;
-  gap: 200px;
+  gap: 100px;
 
   @media (max-width: 1500px) {
     padding: 0 20px;
+    gap: 60px;
+    height: 100px;
+    display: flex;
+  }
+
+  @media (max-width: 1200px) {
+    padding: 0 20px;
+    gap: 60px;
+    height: 130px;
+    display: flex;
+  }
+
+  @media (max-width: 900px) {
+    padding: 0 20px;
     gap: 20px;
     height: 350px;
-    width: 100%;
     display: flex;
     flex-direction: column;
   }
