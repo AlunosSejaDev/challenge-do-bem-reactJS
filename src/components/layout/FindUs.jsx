5ccb7e98c24a31f28563818747fc4e2c8c7e9f25
Bruno Patrício <bpatriciocosta@gmail.com>
2022-08-15 16:54:06 -0300

    ADD Icons contact + estilização deles

--- src/components/layout/FindUs.jsx
+++ src/components/layout/FindUs.jsx
@@ -3,17 +3,21 @@ import styled from 'styled-components'
 import H1 from '../typograph/H1'
 import H3 from '../typograph/H3'
 
+import IconImages from '../icons/IconImages'
+
 const StyledContainerContact = styled.div`
   display: flex;
   justify-content: center;
-  box-sizing: border-box;
-  gap: 500px;
+  gap: 300px;
   margin-top: 40px;
+  align-items: center;
 
-  @media (max-width: 1140px) {
+  @media (max-width: 1400px) {
     display: flex;
     flex-direction: column;
+    gap: 50px;
   }
+
 `
 
 const StyledWhereAreUs = styled.div`
@@ -31,11 +35,31 @@ const StyledContact = styled.div`
   display: flex;
   flex-direction: column;
   gap: 40px;
+  align-items: center;
+`
 
-  @media (max-width: 1140px) {
-    display: flex;
-    text-align: center;
+const StyledWhats = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 290px;
+  gap: 20px;
+`
+
+const StyledEmail = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 290px;
+  gap: 20px;
+
+  @media (max-width: 640px) {
+    display: none;
   }
+`
+const StyledInstagram = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 290px;
+  gap: 20px;
 `
 
 function FindUs () {
@@ -51,10 +75,19 @@ function FindUs () {
       </H3>
     </StyledWhereAreUs>
     <StyledContact>
-      <H1>Contato</H1>
-      <H3>(41) 99727-5152</H3>
-      <H3>challengedobem@gmail.com</H3>
-      <H3>@challengedobem</H3>
+        <H1>Contato</H1>
+        <StyledWhats>
+          <IconImages imageName='WhatsLogo' type='svg' />
+          <H3> (41) 99727-5152</H3>
+        </StyledWhats>
+        <StyledEmail>
+          <IconImages imageName='EmailLogo' type='svg' />
+          <H3> challengedobem@gmail.com</H3>
+        </StyledEmail>
+        <StyledInstagram> 
+          <IconImages imageName='InstagramLogo' type='svg' />
+          <H3> @challengedobem</H3>
+        </StyledInstagram>
     </StyledContact>
 </StyledContainerContact>
 </>
