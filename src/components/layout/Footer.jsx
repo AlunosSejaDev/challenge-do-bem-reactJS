ad4abfd4e1fe8512e4fa4d0e3275d841df6b0a09
Bruno Patrício <bpatriciocosta@gmail.com>
2022-08-18 15:25:03 -0300

    Correção - responsividade do footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components'
 import H5 from '../typograph/H5'
 
-
 const StyledFooter = styled.div`
   display: flex;
   justify-content: center;
@@ -11,9 +10,14 @@ const StyledFooter = styled.div`
   background-color: ${props => props.theme.primary};
   width: 100%;
   height: 105px;
-  margin-top: 2em;
+  margin-top: 4em;
   background: rgba(1, 107, 155, 0.70);
   backdrop-filter: blur(14px);
+
+  @media (max-width: 650px) {
+    height: 230px;
+    margin-top: 8em;
+  }
 
   @media (max-width: 500px) {
     display: flex;
